3759f2818b690507e54ee894cddf2c327b8326f9
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-03-13 09:59:57 +0100

    it 3

--- js/index.js
+++ js/index.js
@@ -2,29 +2,37 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-  // Création des variables en cherchant leur emplacement dans le HTML
+  // Création des variables en cherchant leur emplacement dans le DOM
   const price = product.querySelector('.price span');
   const quantity = product.querySelector('.quantity input');
   const subtotal = product.querySelector('.subtotal span');
   // Calcul de prix * quantité
   let total = price.innerHTML * quantity.value;
   // Integration du résultat dans le DOM
-  subtotal.innerHTML = total
-  return subtotal
+  subtotal.innerHTML = total  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  // Création des variables en cherchant leur emplacement dans le DOM
+  const products = document.getElementsByClassName('product');
+  const total = document.querySelector('#total-value span');
+  const subtotals = document.querySelectorAll('.subtotal span');
+  let totals = 0
+  // on boucle pour calculer le subtotal de chaque ligne
+  for (let i = 0 ; i < products.length ; i++){
+    let prod = products[i]
+    updateSubtotal(prod);
+    totals += +(subtotals[i].innerHTML)
+  }
+  //ITERATION 3
+  total.innerHTML = totals
 }
 
 // ITERATION 4
